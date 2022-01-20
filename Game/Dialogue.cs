da991fa97cecd4a1b358a1173f2600a3aa0ca52d
Chris Nippert <33527729+ChrisNippert@users.noreply.github.com>
2022-01-20 17:11:41 -0500

    Added Lore, NPC, and Dialogue classes. Filled the Lore class.

---
+++ Game/Dialogue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+    class Dialogue
+    {
+        NPC npc;
+        String fileName;
+        int position;
+
+
+        public Dialogue(NPC npc, String fileName)
+        {
+            this.npc = npc;
+            this.fileName = fileName;
+        }
+
+        public void init()
+        {
+
+        }
+    }
+}
--- Game/Lore.cs
+++ Game/Lore.cs
@@ -1,2 +1,60 @@
-public class Lore {
+using System;
+using System.Collections.Generic;
+
+public class Lore
+{
+
+    public static void init()
+    {
+        Dialogue loreMasterTalk = new Dialogue(NPC.NPCs.LoreMaster.npc, "LoreMaster.dialogue");
+        loreMasterTalk.init();
+        Utility.ConsoleFunctions.waitForInput();
+        while (true)
+        {
+            Utility.ConsoleFunctions.cls();
+            Console.WriteLine(listCardNames());
+            Console.WriteLine();
+            Menu cardSelectMenu = new Menu("logic.Card Select\nType the name of the card, or type x to go back", new List<String>());
+            cardSelectMenu.display();
+            if (cardSelectMenu.value == "x")
+            {
+                break;
+            }
+            else
+            {
+                Utility.ConsoleFunctions.cls();
+                Card c = Deck.getCard(cardSelectMenu.value);
+                Console.WriteLine(getCardDetails(c));
+                Utility.ConsoleFunctions.waitForInput();
+            }
+        }
+    }
+
+    public static String listCardNames()
+    {
+        String str = "Cards:\n";
+        for (int i = 0; i < Deck.allCards.getCards().Count; i++)
+        {
+            if (i % 3 == 0 && i > 0)
+            {
+                str += "\n";
+            }
+            str += String.Format("%-20s", Deck.allCards.getCards()[i].name);
+        }
+        return str;
+    }
+
+    public static String getCardDetails(Card c)
+    {
+        return (String.Format(@"
+                Name: % -20s
+                Health: % -2d
+                Attack Power: % -2d
+                Mana Cost: % -2d
+
+                % s
+
+                % s
+                ", c.name, c.hp, c.atk, c.mana, c.toString(), c.desc));
+    }
 }
